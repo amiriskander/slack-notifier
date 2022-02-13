9a138bd1382163363e2596bfc32ef6ab7d2374a3
Amir Iskander <amir.iskander@zlien.com>
2022-02-13 12:51:46 +0200

    Fix accessory type for buttons and new lines at the end of some classes.

--- src/Button.php
+++ src/Button.php
@@ -6,7 +6,7 @@ class Button extends Accessory
 {
     public function __construct(string $url, string $text, ?string $alt = null)
     {
-        parent::__construct('image');
+        parent::__construct('button');
         $this
             ->setUrl($url)
             ->setText(new PlainText($text))
